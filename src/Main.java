993d49011ff784db788315eb50a0b6fc82c207a2
Ali Eren KÖSE <alierenkose@gmail.com>
2024-03-26 05:41:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+// Ali Eren KÖSE    03/2024
+
+public class Main {
+    public static void main(String[] args) {
+        PatikaStore patikaStore = new PatikaStore();
+        patikaStore.run();
+    }
+}
